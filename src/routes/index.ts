78666ae01180aba05906e7bda3db1f8a949733f7
osorbecovk <osorbecov@gmail.com>
2025-10-27 11:13:43 +0600

    add vercel

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,8 +7,11 @@ const globalRouter = Router();
 const corsConfig = {
   origin: [
     "http://localhost:3000",
+    "http://localhost:5172",
     "http://localhost:5173",
     "http://localhost:5174",
+    "http://localhost:5175",
+    "https://plants-front-psi.vercel.app/"
   ],
 };
 
